afe381e1ab387553dae4b6adb4cae5e4567a36fd
Drip <hannah.castro0509@gmail.com>
2026-08-31 17:50:13 +0800

    Update format.js

--- src/lib/format.js
+++ src/lib/format.js
@@ -8,3 +8,9 @@ export const compact = (n) =>
 
 export const shortenAddress = (a) =>
   a && a.length > 14 ? a.slice(0, 6) + "..." + a.slice(-4) : a;
+
+export const pct = (n, digits = 2) =>
+  `${Number(n).toLocaleString("en-US", {
+    minimumFractionDigits: digits,
+    maximumFractionDigits: digits,
+  })}%`;
